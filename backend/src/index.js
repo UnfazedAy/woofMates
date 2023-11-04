68f944afab39e4134e8cf8f16e5a5e2d72910160
UnfazedAy <ayomidesoniyi@gmail.com>
2023-11-04 17:56:04 +0100

    Connected various parts of the app together as the main entry point

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,21 @@ import keys from './config/keys.js';
 import connectDB from './config/db.js';
 import logger from './helpers/logger.js';
 import colors from 'colors';
+import morgan from 'morgan';
+import userRouter from './routes/users.js';
 
-const { PORT, HOST } = keys;
+const { PORT, HOST, NODE_ENV } = keys;
 
 connectDB();
 const app = express();
+
+app.use(express.json());
+
+if (NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/users', userRouter);
 
 const server = app.listen(
   PORT,
