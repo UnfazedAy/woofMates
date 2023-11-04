b7e3ec17aee8b849e7570b9039186abd307101b0
UnfazedAy <ayomidesoniyi@gmail.com>
2023-11-04 17:54:31 +0100

    Added a simple controller for testing

---
+++ backend/src/controllers/users.js
@@ -0,0 +1,11 @@
+import User from '../models/User.js';
+
+const createUser = async (req, res, next) => {
+  const user = await User.create(req.body);
+    res.status(201).json({
+        success: true,
+        data: user,
+    });
+};
+
+export { createUser };
