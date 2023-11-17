57ffdad70af1c7a081917bca74b75751445277c0
UnfazedAy <ayomidesoniyi@gmail.com>
2023-11-17 13:04:53 +0100

    Updated routes for respectives controllers

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,8 +1,25 @@
 import { Router } from 'express';
-import { createUser } from '../controllers/users.js';
+import {
+  createUser,
+  getAllUsers,
+  getUser,
+  updateAvatar,
+} from '../controllers/users.js';
 
 const userRouter = new Router();
 
-userRouter.route('/').post(createUser);
+userRouter
+  .route('/')
+  .post(createUser)
+  .get(getAllUsers);
+
+userRouter
+  .route('/:id')
+  .get(getUser);
+
+// Add a new route for updating the avatar
+userRouter
+  .route('/:id/avatar')
+  .put(updateAvatar);
 
 export default userRouter;
