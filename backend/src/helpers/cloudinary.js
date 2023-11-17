eb8575ba2191a661d20d965e35ee4a0d1c1545f2
UnfazedAy <ayomidesoniyi@gmail.com>
2023-11-17 13:02:33 +0100

    Modified the folder structure to save images

--- backend/src/helpers/cloudinary.js
+++ backend/src/helpers/cloudinary.js
@@ -3,7 +3,6 @@
 import keys from '../config/keys.js';
 import { v2 as cloudinary } from 'cloudinary';
 import logger from './logger.js';
-import axios from 'axios';
 
 const { CLOUDINARY_KEY, CLOUDINARY_SECRET, CLOUDINARY_NAME } =
   keys;
@@ -14,12 +13,10 @@ cloudinary.config({
   api_secret: CLOUDINARY_SECRET,
 });
 
-console.log('Cloudinary Cloud Name:', CLOUDINARY_NAME);
-
 const uploader = async (file, folderName, userName, feildName) => {
   try {
     const res = await cloudinary.uploader.upload(file, {
-      folder: `${folderName}/${userName}_${feildName}`,
+      public_id: `${folderName}/${userName}_${feildName}`,
       resource_type: 'image',
       allowed_formats: ['jpg', 'png', 'jpeg', 'JPG', 'PNG', 'JPEG'],
     });
@@ -30,33 +27,3 @@ const uploader = async (file, folderName, userName, feildName) => {
 };
 
 export default uploader;
-
-// Test function with updated file content handling
-const testUploader = async () => {
-  try {
-    const fileUrl = 'https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=';
-
-    // Fetch the image content as a Buffer using axios
-    const response = await axios.get(fileUrl, {
-      responseType: 'arraybuffer',
-    });
-
-    // Convert the fetched image content into a Buffer
-    const fileBuffer = Buffer.from(response.data);
-
-    const folderName = 'woofMate_users';
-    const userName = 'ayomidesoniyi@gmail.com';
-    const fieldName = 'avatar';
-
-    // Upload the fileBuffer using the uploader function
-    const imageUrl = await uploader(
-      fileBuffer, folderName, userName, fieldName,
-    );
-    console.log('Uploaded image URL:', imageUrl);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-// Run the test function
-testUploader();
