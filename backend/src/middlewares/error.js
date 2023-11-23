afb2897b16efe57909947090e1abf9c5d4de87c7
UnfazedAy <ayomidesoniyi@gmail.com>
2023-11-23 16:46:36 +0100

    Added more error handling

--- backend/src/middlewares/error.js
+++ backend/src/middlewares/error.js
@@ -15,9 +15,11 @@ const errorHandler = (err, req, res, next) => {
 
   // Mongoose duplicate key
   if (err.code === 11000) {
-    const message = 'Duplicate field value entered';
+    const message = Object.keys(err.keyValue).map(
+      (key) => `Duplicate key error, ${key} already exists`);
     error = new ErrorResponse(message, 400);
   }
+
 
   // Mongoose validation error
   if (err.name === 'ValidationError') {
